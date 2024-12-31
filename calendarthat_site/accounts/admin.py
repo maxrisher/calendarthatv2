1f5fe7c3d5b97f166024b66754dccf46b6d8d469
Max Risher <max.risher@gmail.com>
2024-12-31 12:22:06 -0800

    added the new custom user to the admin view and gave them new forms

--- calendarthat_site/accounts/admin.py
+++ calendarthat_site/accounts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin
 
-# Register your models here.
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
+class CustomeUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = ["email", "username", "time_zone_name"]
+
+admin.site.register(CustomUser, CustomeUserAdmin)
