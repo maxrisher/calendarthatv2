875e58b16d52892d3ae880dd60bc8156ab0255d0
Max Risher <max.risher@gmail.com>
2024-12-31 12:30:38 -0800

    setup public database connection to postgres db on railway

--- calendarthat_site/calendarthat_site/settings.py
+++ calendarthat_site/calendarthat_site/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
+import dj_database_url
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -79,10 +80,10 @@ WSGI_APPLICATION = 'calendarthat_site.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_PUBLIC_URL')
+        conn_max_age=600
+    )
 }
 
 
