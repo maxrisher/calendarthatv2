8bd071ff0a08e4034e6a93cb0a059cd36c8c4624
Max Risher <max.risher@gmail.com>
2025-01-20 19:24:13 -0800

    fix admin interface

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,10 +5,10 @@ from django.contrib.auth.admin import UserAdmin
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-class CustomeUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ["email", "username", "time_zone_name"]
 
-admin.site.register(CustomUser, CustomeUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+    first_name = models.CharField(max_length=150, blank=True, null=True)  
+    last_name = models.CharField(max_length=150, blank=True, null=True)  
     time_zone_name = models.CharField(max_length=50, null=True, blank=True)
 
     def __str__(self):
