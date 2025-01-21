92e401342d46e922d4d1f2ec27afb028229b3057
Max Risher <max.risher@gmail.com>
2025-01-20 18:10:37 -0800

    register all models on the admin

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,14 +1,13 @@
 from django.contrib import admin
-from django.contrib.auth import get_user_model
 from django.contrib.auth.admin import UserAdmin
 
 from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser
 
-class CustomeUserAdmin(UserAdmin):
+class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
     list_display = ["email", "username", "time_zone_name"]
 
-admin.site.register(CustomUser, CustomeUserAdmin)
+admin.site.register(CustomUser, CustomUserAdmin)
