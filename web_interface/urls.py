98e553e379327d37d751af3a7906f5d313c8c766
Max Risher <max.risher@gmail.com>
2025-02-02 11:00:20 -0800

    add a new endpoint which requires authentication for chrome extension

--- web_interface/urls.py
+++ web_interface/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('create/', views.create_event_web, name='create_event_web'),
     path('event_status/', views.get_event_status, name='get_event_status'),
     path('download/', views.download_calendar_event, name='download_calendar_event'),
-    path('check_auth/', views.check_auth, name='check_auth')
+    path('check_auth/', views.check_auth, name='check_auth'),
+    path('extension_create/', views.create_event_browser_extension, name='create_event_browser_extension'),
 ]
--- web_interface/views.py
+++ web_interface/views.py
@@ -5,6 +5,7 @@ import logging
 from django.shortcuts import render
 from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
 
 from event_creator.models import Event
 from event_creator.new_event import EventBuilder
@@ -19,7 +20,6 @@ def home(request):
     """
     return render(request, 'web_interface/home.html')
 
-@csrf_exempt # NB: Mallicious websites will be able to make requests through our users to this endpoint
 async def create_event_web(request):
     """
     [INTERFACE] Initiates asynchronous calendar event creation process
@@ -120,3 +120,40 @@ async def check_auth(request):
         return JsonResponse({"message": "Authenticated"}, status=200) 
     else:
         return JsonResponse({"error": "Not authenticated"}, status=401) 
+
+@login_required    
+@csrf_exempt # NB: Mallicious websites will be able to make requests through our users to this endpoint
+async def create_event_browser_extension(request):
+    """
+    [INTERFACE] Initiates asynchronous calendar event creation process
+    [IN] HTTP POST request with event_text and optional authenticated user
+    [OUT] JSON response with event UUID for tracking
+    """
+    logger.debug(f"""
+    Request Method: {request.method}
+    Request Path: {request.path}
+    Request GET: {request.GET}
+    Request POST: {request.POST}
+    Request Headers: {dict(request.headers)}
+    """)
+    user = await request.auser()
+    event_text = request.POST.get('event_text', '')
+
+    logger.info(f"New event creation requested by user {user.id if user.is_authenticated else 'anonymous'}")
+    
+    event_uuid = uuid.uuid4()
+
+    await Event.objects.acreate(
+        uuid=event_uuid, 
+        custom_user=user if user.is_authenticated else None, 
+        user_input=event_text
+        )
+
+    new_event = EventBuilder(event_uuid, event_text)
+    asyncio.create_task(new_event.formalize())
+    
+    logger.info(f"Event creation initiated with UUID: {event_uuid}")
+    
+    return JsonResponse({
+        "event_uuid": str(event_uuid)
+    })
