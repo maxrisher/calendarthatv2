971654377544fc8d0f307037ee73e2f35c51ac4a
Max Risher <max.risher@gmail.com>
2025-02-16 20:20:52 -0800

    new signup form

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,11 +1,22 @@
+import pytz
+
+from django import forms
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+
 from .models import CustomUser
 
+TIMEZONE_CHOICES = [(tz, tz) for tz in pytz.all_timezones]
+
 class CustomUserCreationForm(UserCreationForm):
+    time_zone_name = forms.ChoiceField(
+        choices=TIMEZONE_CHOICES,
+        required=True,
+        widget=forms.Select(attr={'class': 'form-control'})
+    )
 
     class Meta:
         model = CustomUser
-        fields = ("username", "email")
+        fields = ("email", "time_zone_name")
 
 class CustomUserChangeForm(UserChangeForm):
 
