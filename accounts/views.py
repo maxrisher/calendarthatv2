025846b17ca8f7e0080a9952f7af8d0d82ce68f7
Max Risher <max.risher@gmail.com>
2025-02-17 10:46:50 -0800

    added timezone settings

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
+from django.urls import reverse
+
+from asgiref.sync import sync_to_async
+
 from .forms import UserSettingsForm
 
 @login_required
@@ -9,8 +13,8 @@ async def user_settings(request):
     if request.method == 'POST':
         form = UserSettingsForm(request.POST, instance=user)
         if form.is_valid():
-            form.save()
-            return redirect('user_settings') # check this
+            await sync_to_async(form.save)()
+            return redirect(reverse('user_settings')) # check this
         
     else:
         form = UserSettingsForm(instance=user)
